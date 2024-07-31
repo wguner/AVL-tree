78ba038426e55b95c3284a6956ed5ed45b502681
Setenay <setenayguner@gmail.com>
2024-07-31 12:18:34 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Binary Search Tree implementation 
+ *  - heavily lifted from https://gist.github.com/mgechev/5911348
+ *
+ * Simple integer keys and basic operations BST
+ *
+ */
+
+#include <iostream>
+//#include "BST.h"	//BST implementation
+#include "AVLNode.h" // AVLNode class
+#include "AVLTree.h"
+
+/*
+ * Main function - includes enabling test mode
+ */
+int main( int argc, char* argv[] ) {
+    std::cout << " [x] Running main program - nothing to do!" << std::endl;
+    std::cout << "     Since it's quiet, here's a flower..." << std::endl;
+    std::cout << std::endl;
+    return 0;
+}
